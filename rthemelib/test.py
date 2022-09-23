7aa0b21892c88cc1f73169d6421d15d0e421f52a
Cameron <pizzalovingnerd@teknik.io>
2022-09-22 18:49:29 -0700

    added nord theme

--- rthemelib/test.py
+++ rthemelib/test.py
@@ -3,6 +3,6 @@ import rthemelib
 import theme_classes as tc
 
 theme = tc.Theme()
-theme.parse_yaml("/home/cameron/PycharmProjects/project-beautiful/themes/theme_template.yml")
+theme.parse_yaml("/home/cameron/PycharmProjects/project-beautiful/themes/nord.yml")
 rthemelib.apply_theme(theme, "main", "light")
 print(rthemelib.get_theme_list())
