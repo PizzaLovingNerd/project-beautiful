4e94733c7da95e2a639d3233b64c62fdef6561d7
Jamie Murphy <hello@itsjamie.dev>
2022-10-04 23:21:29 -0700

    test.bash: Don't hardcode as many values

--- rthemed/test.bash
+++ rthemed/test.bash
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-sudo rm -rf /usr/lib64/python3.10/site-packages/rthemelib
-sudo rm -rf /usr/lib64/python3.10/site-packages/rthemegtk
-sudo rm -rf /usr/lib64/python3.10/site-packages/rthemed
+# Get the current Python version in the format MAJOR.MINOR
+PYTHON_VERSION=$(python -c 'import platform; major, minor, patch = platform.python_version_tuple(); print("{}.{}".format(major, minor));')
+SITE_PACKAGES=$(python -m site --user-site)
+
+echo "Using Python $PYTHON_VERSION, with site packages at $SITE_PACKAGES"
+
+rm -rf $SITE_PACKAGES/rthemelib
+rm -rf $SITE_PACKAGES/rthemegtk
+rm -rf $SITE_PACKAGES/rthemed
 sudo rm -rf /usr/share/rtheme
 sudo rm /usr/share/glib-2.0/schemas/io.risi.rtheme.gschema.xml
 
-sudo mkdir -p /usr/lib64/python3.10/site-packages/
+mkdir -p $SITE_PACKAGES
 sudo mkdir -p /usr/share/rtheme
 
-sudo cp -a ../rthemelib /usr/lib64/python3.10/site-packages/rthemelib
-sudo cp -a ../rthemegtk /usr/lib64/python3.10/site-packages/rthemegtk
-sudo cp -a ../rthemed /usr/lib64/python3.10/site-packages/rthemed
+cp -a ../rthemelib $SITE_PACKAGES/rthemelib
+cp -a ../rthemegtk $SITE_PACKAGES/rthemegtk
+cp -a ../rthemed $SITE_PACKAGES/rthemed
 sudo cp -a ../data/io.risi.rtheme.gschema.xml /usr/share/glib-2.0/schemas/io.risi.rtheme.gschema.xml
 sudo cp -a ../data/*.desktop /usr/share/applications/
 
